d48a2b3fa228597927d2dd4048bd02d012eb4dac
Reijhanniel Jearl Campos <devcsrj@apache.org>
2018-12-26 11:06:29 +0800

    :tada: Initial commit

---
+++ src/main/java/devcsrj/mazerunner/MazerunnerApplication.java
@@ -0,0 +1,14 @@
+package devcsrj.mazerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazerunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazerunnerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/devcsrj/mazerunner/MazerunnerApplicationTests.java
@@ -0,0 +1,17 @@
+package devcsrj.mazerunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MazerunnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
